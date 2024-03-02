aaa5556baf7e5741d03674e26f5090fda1054e42
ISHRAQ01 <ISHRAQ0641@GMAIL.COM>
2024-03-02 16:14:26 +0530

    Salaryslip

--- LAB PRACTISE/Salaryslip.java
+++ LAB PRACTISE/Salaryslip.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 import java.text.NumberFormat;
 import java.util.Locale;
 public class Salaryslip{
-    static void salaryFormat(int sal,Locale l){
+    static void salaryFormat(double sal,Locale l){
         NumberFormat nf=NumberFormat.getCurrencyInstance(l);
         String temp=nf.format(sal);
-        System.out.println("THIS IS THE FORMATTED SALARY:" +temp);
+        System.out.println("THIS IS THE FORMATTED SALARY: " +temp);
+    }
+    static double hra(double salary){
+        return salary*0.3;
+    }
+    static double da(double salary){
+        return salary*0.2;
+    }
+    static double ta(double salary){
+        return salary*0.1;
+    }
+    static double net_Salary(double salary,double pf){
+        return (salary+hra(salary)+da(salary)+ta(salary)-pf);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("PLEASE SELECT YOUR LANGUAGE:");
-        System.out.println("ENTER 1 FOR HINDI, 2 FOR ENGLISH, 3 FOR FRENCH");
+        System.out.println("ENTER 1 FOR HINDI, 2 FOR ENGLISH, 3 FOR FRENCH ");
         int loc = sc.nextInt();
         Locale locale;
         if (loc == 1) {
@@ -23,9 +35,10 @@ public class Salaryslip{
             System.out.println("INVALID INPUT, NOW USING ENGLISH AS DEFAULT");
             locale =  Locale.of("en", "US");
         }
-        System.out.println("ENTER THE BASIC SALARY: ");
-        int n=sc.nextInt();
-        salaryFormat(n, locale);
+        System.out.println("ENTER THE SALARY OF AN EMPLOYEE: ");
+        double salary=sc.nextDouble();
+        double sal=net_Salary(salary,1500);
+        salaryFormat(sal, locale);
         sc.close();
 
     }

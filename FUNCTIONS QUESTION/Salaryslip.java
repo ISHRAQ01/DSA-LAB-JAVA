75d159c09294ee0a8281132ba2517e6ca56b9735
ISHRAQ01 <ISHRAQ0641@GMAIL.COM>
2024-03-02 12:22:46 +0530

    Salaryslip

---
+++ FUNCTIONS QUESTION/Salaryslip.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+public class Salaryslip{
+    static void salaryFormat(int sal,Locale l){
+        NumberFormat nf=NumberFormat.getCurrencyInstance(l);
+        String temp=nf.format(sal);
+        System.out.println("THIS IS THE FORMATTED SALARY:" +temp);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("PLEASE SELECT YOUR LANGUAGE:");
+        System.out.println("ENTER 1 FOR HINDI, 2 FOR ENGLISH, 3 FOR FRENCH");
+        int loc = sc.nextInt();
+        Locale locale;
+        if (loc == 1) {
+            locale = Locale.of("hi", "IN");
+        } else if (loc == 2) {
+            locale =  Locale.of("en", "US");
+        } else if (loc == 3) {
+            locale =  Locale.of("fr", "FR");
+        } else {
+            System.out.println("INVALID INPUT, NOW USING ENGLISH AS DEFAULT");
+            locale =  Locale.of("en", "US");
+        }
+        System.out.println(locale);
+        System.out.println("ENTER THE BASIC SALARY: ");
+        int n=sc.nextInt();
+        salaryFormat(n, locale);
+        sc.close();
+
+    }
+}
+
